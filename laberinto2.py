9e38a6f690826d66fb5e62cbf2389b943339fa96
andres salazar <iandrescali@gmail.com>
2023-04-25 14:28:43 -0500

    Update laberinto2.py

--- laberinto2.py
+++ laberinto2.py
@@ -65,9 +65,13 @@ for row in range(1, NUM_ROWS - 1):
                 maze[row-1][col] == 0 or maze[row+1][col] == 0):
                 maze[row][col] = 0
 
+# Calcular las coordenadas x e y en la pantalla que corresponden a la celda del laberinto donde se encuentra la puerta de entrada
+entry_x = entry_col * CELL_SIZE + CELL_SIZE // 2
+entry_y = entry_row * CELL_SIZE + CELL_SIZE // 2
+
 # Definir posición inicial del jugador
-player_x = SCREEN_WIDTH // 2
-player_y = SCREEN_HEIGHT // 2
+player_x = entry_x
+player_y = entry_y
 
 # Definir tamaño del jugador
 player_size = CELL_SIZE
@@ -93,6 +97,7 @@ class Enemy:
         self.y = y
         self.size = CELL_SIZE
         self.speed = CELL_SIZE + CELL_MARGIN
+        self.lives = 3
 
     def move(self):
         row = self.y // (CELL_SIZE + CELL_MARGIN)
@@ -123,6 +128,19 @@ enemies.append(enemy1)
 enemy2 = Enemy(x2, y2)
 enemies.append(enemy2)
 
+# Definir número inicial de vidas
+player_lives = 3
+
+# Definir fuente para el texto
+font = pygame.font.Font(None, 36)
+
+# Definir tamaño del jugador
+player_size_x = CELL_SIZE
+player_size_y = CELL_SIZE
+
+# Definir estado del jugador (0 = normal, 1 = defensa)
+player_state = 0
+
 # Bucle principal
 running = True
 while running:
@@ -134,13 +152,43 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT and player_x > 0:
                 player_x -= player_speed
-            elif event.key == pygame.K_RIGHT and player_x < SCREEN_WIDTH - player_size:
+                player_direction = 'left'
+            elif event.key == pygame.K_RIGHT and player_x < SCREEN_WIDTH - player_size_x:
                 player_x += player_speed
+                player_direction = 'right'
             elif event.key == pygame.K_UP and player_y > 0:
                 player_y -= player_speed
-            elif event.key == pygame.K_DOWN and player_y < SCREEN_HEIGHT - player_size:
+                player_direction = 'up'
+            elif event.key == pygame.K_DOWN and player_y < SCREEN_HEIGHT - player_size_y:
                 player_y += player_speed
-                
+                player_direction = 'down'
+            elif event.key == pygame.K_SPACE:
+                if player_state == 0:
+                    # Cambiar a estado de defensa
+                    player_state = 1
+                    # Aumentar el tamaño del jugador en la dirección que está mirando
+                    if player_direction == 'left':
+                        player_size_x *= 2
+                        player_x -= CELL_SIZE
+                    elif player_direction == 'right':
+                        player_size_x *= 2
+                    elif player_direction == 'up':
+                        player_size_y *= 2
+                        player_y -= CELL_SIZE
+                    elif player_direction == 'down':
+                        player_size_y *= 2
+                else:
+                    # Cambiar a estado normal
+                    player_state = 0
+                    # Restablecer el tamaño del jugador
+                    if player_direction in ['left', 'right']:
+                        player_size_x = CELL_SIZE
+                        if player_direction == 'left':
+                            player_x += CELL_SIZE
+                    else:
+                        player_size_y = CELL_SIZE
+                        if player_direction == 'up':
+                            player_y += CELL_SIZE
     # Cargar una imagen como textura
     texture = pygame.image.load('texture.jpg')
     
@@ -187,10 +235,77 @@ while running:
     # Actualizar la ventana
     pygame.display.update()
 
+    # Verificar si algún enemigo ha tocado al jugador
+    for enemy in enemies:
+        if (player_x < enemy.x + enemy.size and
+            player_x + player_size > enemy.x and
+            player_y < enemy.y + enemy.size and
+            player_y + player_size > enemy.y):
+            # Restar una vida al jugador
+            player_lives -= 1
+            print(f'¡Perdiste una vida! Vidas restantes: {player_lives}')
+            # Verificar si el jugador aún tiene vidas restantes
+            if player_lives == 0:
+                running = False
+                # Crear superficie con el texto de derrota
+                text = font.render("¡Perdiste!", True, (255, 0, 0))
+                # Calcular posición del texto en la pantalla
+                text_x = SCREEN_WIDTH // 2 - text.get_width() // 2
+                text_y = SCREEN_HEIGHT // 2 - text.get_height() // 2
+                # Dibujar un rectángulo detrás del texto
+                pygame.draw.rect(screen, (0, 0, 0), (text_x - 10, text_y - 10,
+                                                     text.get_width() + 20,
+                                                     text.get_height() + 20))
+                # Dibujar el texto en la pantalla
+                screen.blit(text, (text_x, text_y))
+                # Actualizar la ventana
+                pygame.display.update()
+                # Esperar antes de salir del juego
+                pygame.time.wait(3000)
+                print("¡Perdiste!")
+                
+    # Verificar si algún enemigo ha tocado al jugador mientras está en estado de defensa
+    if player_state == 1:
+        for enemy in enemies[:]:
+            if (player_x < enemy.x + enemy.size and
+                player_x + player_size_x > enemy.x and
+                player_y < enemy.y + enemy.size and
+                player_y + player_size_y > enemy.y):
+                # Restar una vida al enemigo
+                enemy.lives -= 1
+                print(f'¡Le quitaste una vida al enemigo! Vidas restantes: {enemy.lives}')
+                # Verificar si el enemigo aún tiene vidas restantes
+                if enemy.lives == 0:
+                    enemies.remove(enemy)
+                    print('¡Eliminaste a un enemigo!')
     # Verificar si el jugador llegó a la salida
     if player_x // (CELL_SIZE + CELL_MARGIN) == exit_col and player_y // (CELL_SIZE + CELL_MARGIN) == exit_row:
         running = False
+        # Crear superficie con el texto de victoria
+        text = font.render("¡Ganaste!", True, (0, 255, 0))
+        # Calcular posición del texto en la pantalla
+        text_x = SCREEN_WIDTH // 2 - text.get_width() // 2
+        text_y = SCREEN_HEIGHT // 2 - text.get_height() // 2
+        # Dibujar un rectángulo detrás del texto
+        pygame.draw.rect(screen, (0, 0, 0), (text_x - 10, text_y - 10,
+                                             text.get_width() + 20,
+                                             text.get_height() + 20))
+        # Dibujar el texto en la pantalla
+        screen.blit(text, (text_x, text_y))
+        # Actualizar la ventana
+        pygame.display.update()
+        # Esperar antes de salir del juego
+        pygame.time.wait(3000)
         print("¡Ganaste!")
 
+    # Crear superficie con el texto de las vidas del jugador
+    text = font.render(f'Vidas: {player_lives}', True, (255, 255, 255))
+    # Dibujar un rectángulo detrás del texto
+    pygame.draw.rect(screen, (0, 0, 0), (10 - 5, 10 - 5,
+                                         text.get_width() + 10,
+                                         text.get_height() + 10))
+    # Dibujar el texto en la pantalla
+    screen.blit(text, (10, 10))
+    pygame.display.update()
 # Salir de Pygame
 pygame.quit()
